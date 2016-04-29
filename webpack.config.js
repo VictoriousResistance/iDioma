7aa1943ef5258ed9e748a45d1bb99368f7b4c0bd
Reina <reinameic@gmail.com>
2016-04-29 15:14:58 -0700

    Remove react native folder, add react and webpack dependencies (#13)
    * Add react dependencies, webpack, and babel loader

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+require('webpack');
+
+module.exports = {
+  entry: './client/index.js',
+  output: { path: __dirname, filename: '/client/bundle.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+    ],
+  },
+};
