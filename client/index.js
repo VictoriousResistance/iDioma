77bba5524f8e161efb05437faf692953d99c4efb
Reina Mei <reinameic@gmail.com>
2016-04-29 16:32:06 -0700

    Add static initial state

--- client/index.js
+++ client/index.js
@@ -1,4 +1,103 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+window.__INITIAL_STATE__ = {
+  user: {
+    firstName: 'Lach',
+    lastName: 'Zester',
+    canTeach: [
+      ['French', 'fluent'],
+      ['English', 'native'],
+    ],
+    willLearn: [
+      ['German', 'basic'],
+    ],
+    interests: [
+      'computer science', 'house music',
+    ],
+  },
+  connections: [
+    {
+      firstName: 'Mo',
+      lastName: 'Naqvi',
+      canTeach: [
+        ['German', 'fluent'],
+      ],
+      willLearn: [
+        ['French', 'basic'],
+      ],
+      interests: [
+        'music',
+      ],
+    },
+    {
+      firstName: 'Mo',
+      lastName: 'Naqvi',
+      canTeach: [
+        ['German', 'fluent'],
+      ],
+      willLearn: [
+        ['French', 'basic'],
+      ],
+      interests: [
+        'music',
+      ],
+    },
+    {
+      firstName: 'Mo',
+      lastName: 'Naqvi',
+      canTeach: [
+        ['German', 'fluent'],
+      ],
+      willLearn: [
+        ['French', 'basic'],
+      ],
+      interests: [
+        'music',
+      ],
+    },
+  ],
+  matches: [
+    {
+      firstName: 'Mo',
+      lastName: 'Naqvi',
+      canTeach: [
+        ['German', 'fluent'],
+      ],
+      willLearn: [
+        ['French', 'basic'],
+      ],
+      interests: [
+        'music',
+      ],
+    },
+    {
+      firstName: 'Mo',
+      lastName: 'Naqvi',
+      canTeach: [
+        ['German', 'fluent'],
+      ],
+      willLearn: [
+        ['French', 'basic'],
+      ],
+      interests: [
+        'music',
+      ],
+    },
+    {
+      firstName: 'Mo',
+      lastName: 'Naqvi',
+      canTeach: [
+        ['German', 'fluent'],
+      ],
+      willLearn: [
+        ['French', 'basic'],
+      ],
+      interests: [
+        'music',
+      ],
+    },
+  ],
+};
+
 render((<div>Hi</div>), document.getElementById('app'));
