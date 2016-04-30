d35d1613a9183517f93fc48a201dc6cbd8871477
Reina Mei <reinameic@gmail.com>
2016-04-30 16:41:09 -0700

    Enable browser refresh

--- server/index.js
+++ server/index.js
@@ -1,15 +1,13 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
 
-// app.use(rewrite('/*', '/'));
-
 app.use(express.static(__dirname + '/../client'));
 
-// app.get('*', function (request, response){
-//   response.sendFile(__dirname + '/../client/index.html');
-// });
-
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, '..', 'client', 'index.html'))
+});
 
 app.listen(3000, function() {
   console.log('iDioma listening on http://localhost:3000, Ctrl + C to stop');
