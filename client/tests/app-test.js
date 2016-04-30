eb3835598d0c6a00974eb5cb8cef576383798579
Ashwin Ravi <ashravi@Ashwins-MacBook-Pro.local>
2016-04-29 21:50:58 -0700

    Completed testing setup and wrote App component test

---
+++ client/tests/app-test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TestUtils from 'react-addons-test-utils';
+
+import App from '../app.jsx';
+
+describe('app', function () {
+  it('renders without issues', function () {
+    const app = TestUtils.renderIntoDocument(<App/>);
+    expect(app).toBeDefined();
+  });
+});
