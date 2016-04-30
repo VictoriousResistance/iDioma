49c01f083a2be0f02626e7f3240b29217ad1c6a0
Reina Mei <reinameic@gmail.com>
2016-04-29 19:44:53 -0700

    Enable client-side routing

--- client/components/LeftRail.jsx
+++ client/components/LeftRail.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const LeftRail = () => (
-  <div>LeftRail</div>
+  <div>
+    <Link to="/connections">My Connections</Link>
+    <Link to="/requests">Connection Requests</Link>
+    <Link to="/matches">Potential Connections</Link>
+  </div>
 );
 
 export default LeftRail;
