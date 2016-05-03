73ad1e6ef5705e003fe42302ca819117c5bdc68f
Reina Mei <reinameic@gmail.com>
2016-05-02 19:41:40 -0700

    Change LeftRail component to TopBar

---
+++ client/components/TopBar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const TopBar = () => (
+  <ul id="top-bar">
+    <li className="top-bar-item" id="logo">
+      <a href="/home">iDioma</a>
+    </li>
+    <li className="top-bar-item">
+      <Link to="/home/requests">Connection Requests</Link>
+    </li>
+    <li className="top-bar-item">
+      <Link to="/home/connections">My Connections</Link>
+    </li>
+    <li className="top-bar-item">
+      <Link to="/home/matches">Matches</Link>
+    </li>
+    <div className="profile">
+      <li className="top-bar-item">
+        <Link to="/home/profile">My Profile</Link>
+      </li>
+      <li className="top-bar-item">
+        <a href="/logout">Sign out</a>
+      </li>
+    </div>
+  </ul>
+);
+
+export default TopBar;
