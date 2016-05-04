6e8f77ff0c8c28a3498398028ce717dcd149c598
Reina Mei <reinameic@gmail.com>
2016-05-04 16:31:24 -0700

    Improve structure in match component

--- client/components/Matches.jsx
+++ client/components/Matches.jsx
@@ -3,25 +3,31 @@ import Language from './Language.jsx';
 
 const Match = ({ match, onConnectClick, onHideClick }) => (
   <div className={match.willUnmount ? "person animated fadeOut" : "person"}>
+
     <div className="profile-picture-container">
       <img className="profile-picture" src={match.photoUrl}/>
     </div>
-    <div>{match.firstName + ' ' + match.lastName}</div>
-    <div>{match.description}</div>
-    <div>
-      <span>{'Can offer: '}</span>
-      <div className="languages">
-          {match.canTeach.map((language, i) => (<Language key={i} level={language[1]} name={language[0]} />))}
+
+    <div className="text-container">
+      <div>{match.firstName + ' ' + match.lastName}</div>
+      <div>{match.description}</div>
+      <div>
+        <span>{'Can offer: '}</span>
+        <div className="languages">
+            {match.canTeach.map((language, i) => (<Language key={i} level={language[1]} name={language[0]} />))}
+        </div>
+      </div>
+      <div>
+        <span>{'Wants to learn: '}</span>
+        <div className="languages">
+            {match.willLearn.map((language, i) => (<Language key={i} level={language[1]} name={language[0]} />))}
+        </div>
       </div>
     </div>
-    <div>
-      <span>{'Wants to learn: '}</span>
-      <div className="languages">
-          {match.willLearn.map((language, i) => (<Language key={i} level={language[1]} name={language[0]} />))}
-      </div>
+    <div className="button-container">
+      <a href="#" className="action" onClick={() => { onConnectClick(match.id); }}>Connect</a>
+      <a href="#" className="action" onClick={() => { onHideClick(match.id); }}>Hide</a>
     </div>
-    <a href="#" className="action" onClick={() => { onConnectClick(match.id); }}>Connect</a>
-    <a href="#" className="action" onClick={() => { onHideClick(match.id); }}>Hide</a>
   </div>
 );
 
