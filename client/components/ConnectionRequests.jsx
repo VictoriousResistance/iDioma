caaa62b1d4addcebe8ff7ade53744eb37343168b
Reina Mei <reinameic@gmail.com>
2016-05-03 21:14:24 -0700

    Add animation to accept button

--- client/components/ConnectionRequests.jsx
+++ client/components/ConnectionRequests.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 
-const ConnectionRequest = ({ connectionRequest, onAcceptClick, onDeclineClick }) => (
-  <div className="person">
-    <div>{connectionRequest.firstName + ' ' + connectionRequest.lastName}</div>
-    <div>{connectionRequest.canTeach}</div>
-    <div>{connectionRequest.willLearn}</div>
-    <div>{connectionRequest.description}</div>
-    <a href="#" className="action" onClick={() => { onAcceptClick(connectionRequest.id); }}>Accept</a>
-    <a href="#" className="action" onClick={() => { onDeclineClick(connectionRequest.id); }}>Decline</a>
-  </div>
-);
+class ConnectionRequest extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      className: "person",
+    };
+  }
+  render() {
+
+    const { connectionRequest, onAcceptClick, onDeclineClick } = this.props;
+
+    return (
+      <div className={this.state.className}>
+        <div>{connectionRequest.firstName + ' ' + connectionRequest.lastName}</div>
+        <div>{connectionRequest.canTeach}</div>
+        <div>{connectionRequest.willLearn}</div>
+        <div>{connectionRequest.description}</div>
+        <a href="#" className="action"
+          onClick={() => {
+            this.setState({
+              className: "animated fadeOut person",
+            });
+            setTimeout(() => { onAcceptClick(connectionRequest.id); }, 120);
+          }}
+        >Accept</a>
+        <a href="#" className="action" onClick={() => { onDeclineClick(connectionRequest.id); }}>Decline</a>
+      </div>
+    );
+  }
+}
 
 const ConnectionRequests = ({ connectionRequests, onAcceptClick, onDeclineClick }) => (
   <div>
