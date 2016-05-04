057f4041639ae74e1e12f06f9aa29864076f2bd6
Reina Mei <reinameic@gmail.com>
2016-05-04 12:32:29 -0700

    Organize css sections, refactor match component

--- client/components/Matches.jsx
+++ client/components/Matches.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Language from './Language.jsx';
 
 const Match = ({ match, onConnectClick, onHideClick }) => (
   <div className={match.willUnmount ? "person animated fadeOut" : "person"}>
     <div>{match.firstName + ' ' + match.lastName}</div>
-    <div>{match.canTeach}</div>
+    <span>{'Languages ' + match.firstName + ' can offer:'}</span>
+    <span>
+        {match.canTeach.map((language, i) => (<Language key={i} level={language[1]} name={language[0]} />))}
+    </span>
     <div>{match.willLearn}</div>
     <div>{match.description}</div>
     <a href="#" className="action" onClick={() => { onConnectClick(match.id); }}>Connect</a>
