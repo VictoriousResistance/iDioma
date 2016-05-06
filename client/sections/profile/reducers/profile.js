574b282d01ebfdcd8442d3c752088052f3e85911
Reina Mei <reinameic@gmail.com>
2016-05-06 14:31:11 -0700

    Improve logic on profile to handle duplicates

--- client/sections/profile/reducers/profile.js
+++ client/sections/profile/reducers/profile.js
@@ -3,29 +3,65 @@ const profile = (state = {}, action) => {
 
     case 'ADD_LEARN':
       const willLearn = state.willLearn;
-      var duplicate = false;
-      willLearn.forEach((language) => {
-        if (language[0] === action.language[0] && language[1] === action.language[1]) {
-          duplicate = true;
+      var duplicate;
+      var halfDuplicate;
+      var newWillLearn = []; 
+      var replacedIndex;
+      willLearn.forEach((language, index) => {
+        if (language[0] === action.language[0]) {
+          if (language[1] === action.language[1]) {
+            duplicate = true;
+          } else {
+            halfDuplicate = true;
+            replacedIndex = index;
+          }
         }
-      })
+      });
       if (duplicate) {
         return state;
+      }
+      if (halfDuplicate) {
+        for (var i = 0; i < willLearn.length; i++) {
+          if (i !== replacedIndex) {
+            newWillLearn.push(willLearn[i]);
+          } else {
+            newWillLearn.push(action.language);
+          }
+        }
+        return Object.assign({}, state, { willLearn: newWillLearn });
       }
       return Object.assign({}, state, { willLearn: willLearn.concat([action.language]) });
 
     case 'ADD_TEACH':
       const canTeach = state.canTeach;
-      var duplicate = false;
-      canTeach.forEach((language) => {
-        if (language[0] === action.language[0] && language[1] === action.language[1]) {
-          duplicate = true;
+      var duplicate;
+      var halfDuplicate;
+      var newCanTeach = [];
+      var replacedIndex;
+      canTeach.forEach((language, index) => {
+        if (language[0] === action.language[0]) {
+          if (language[1] === action.language[1]) {
+            duplicate = true;
+          } else {
+            halfDuplicate = true;
+            replacedIndex = index;
+          }
         }
       }); 
       if (duplicate) {
         return state;
       }
-      return Object.assign({}, state, { canTeach: canTeach.concat([action.language])  });
+      if (halfDuplicate) {
+        for (var i = 0; i < canTeach.length; i++) {
+          if (i !== replacedIndex) {
+            newCanTeach.push(canTeach[i]);
+          } else {
+            newCanTeach.push(action.language);
+          }
+        }
+        return Object.assign({}, state, { canTeach: newCanTeach });
+      }
+      return Object.assign({}, state, { canTeach: canTeach.concat([action.language]) });
 
     case 'REMOVE_LEARN':
       const newLearn = [];
