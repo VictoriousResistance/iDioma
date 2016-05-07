8a079fde6a2888c38fc582174bc46ae8f5051c43
Reina Mei <reinameic@gmail.com>
2016-05-07 12:20:55 -0700

    Check for duplicate before dispatching addLearn

--- client/sections/profile/components/WillLearn.jsx
+++ client/sections/profile/components/WillLearn.jsx
@@ -35,7 +35,18 @@ class WillLearn extends Component {
           <option value="advanced">Advanced</option>
         </select>
 
-        <button onClick={() => { onAddLearnClick([this.refs['learn'].value, this.refs['learn-level'].value]); }}>Add</button>
+        <button
+          onClick={() => {
+            const isDuplicate = willLearn.reduce((isDup, language) => {
+              return isDup || ((language[0] === this.refs['learn'].value) && (language[1] === this.refs['learn-level'].value));
+            }, false);
+            if (!isDuplicate) {
+              onAddLearnClick([this.refs['learn'].value, this.refs['learn-level'].value]);
+            }
+          }}
+        >
+          Add
+        </button>
 
       </div>
     );
