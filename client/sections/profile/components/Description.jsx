51c4d6519db56e22cfe28bb3cda9f0349af230ac
Reina Mei <reinameic@gmail.com>
2016-05-06 20:44:23 -0700

    Show description char count while typing

--- client/sections/profile/components/Description.jsx
+++ client/sections/profile/components/Description.jsx
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
 class Description extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCount: false,
+    };
+  }
   render() {
     const { description, updateDescription } = this.props;
+    const charLimit = 100;
+    var charCount = this.state.showCount ? (<div>{ 'Characters left: ' + (charLimit - this.refs.description.innerText.length) }</div>) : null;
     return (
       <div className="profile-section">
 
@@ -20,16 +28,27 @@ class Description extends Component {
               e.preventDefault();
               this.refs.description.blur();
             }
-            if (this.refs.description.innerText.length > 100 && e.which !== 8) {
+            if (this.refs.description.innerText.length > charLimit && e.which !== 8) {
               e.preventDefault();
             }
           }}
           onPaste={(e) => {
             e.preventDefault();
           }}
+          onFocus={() => {
+            this.setState({
+              showCount: true,
+            });
+          }}
+          onBlur={() => {
+            this.setState({
+              showCount: false,
+            });
+          }}
         >
           {description}
         </div>
+        {charCount}
 
       </div>
     );
