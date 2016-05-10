7307088242437dc2e9e8741ece1e274ba2937592
Reina Mei <reinameic@gmail.com>
2016-05-10 16:54:36 -0700

    Correct format of self.canTeach

--- server/db/controllers/getMatchesGivenSelfAndOffset.js
+++ server/db/controllers/getMatchesGivenSelfAndOffset.js
@@ -1,13 +1,14 @@
-const Sequelize = require('sequelize');
+const db = require('../db.js');
 
 module.exports = (self, offSet) => {
-  const queryString = `SELECT user_id FROM UserLanguageLevel 
+  const queryString = `SELECT UserLanguageLevel.User FROM UserLanguageLevel 
                          INNER JOIN LanguageLevel 
                            ON UserLanguageLevel.LanguageLevel = LanguageLevel.id
                          INNER JOIN Language
                            ON LanguageLevel.Language = Language.id
                          INNER JOIN Level
                            ON LanguageLevel.Level = Level.id
-                         WHERE Language.name IN ${self.canTeach} AND Level.name IN ['fluent', 'native']`
-  // sequelize.query();
+                         WHERE Language.name IN (${self.canTeach.map(language => language[0]).join(',')}) AND Level.name IN ('fluent', 'native')
+                      `;
+  // db.query();
 };
