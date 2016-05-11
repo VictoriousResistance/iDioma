8c4eecf93ff4bab98631c3f2c27bbc24e6fde142
Reina Mei <reinameic@gmail.com>
2016-05-11 10:27:01 -0700

    Implement getUserIdsGivenSelfIdAndRelationType

--- server/db/controllers/getUserIdsGivenSelfIdAndRelationshipType.js
+++ server/db/controllers/getUserIdsGivenSelfIdAndRelationshipType.js
@@ -1,16 +1,18 @@
+const db = require('../db.js');
+const Sequelize = require('sequelize');
+
 const getUserIdsGivenSelfIdAndRelationshipType = (selfId, relationshipType) => {
-
+  const queryStr = `SELECT users.id FROM users 
+                      INNER JOIN relationships 
+                        ON users.id = relationships.user1Id OR users.id = relationships.user2Id 
+                      WHERE users.id = ${selfId} AND relationships.type = ${relationshipType}
+                    `;
+  return db.query(queryStr, { type: Sequelize.QueryTypes.SELECT });
 };
 
-exports.getConnections = (selfId) => {
-  return getUserIdsGivenSelfIdAndRelationshipType(selfId, 'connection');
-};
+exports.getConnections = (selfId) => getUserIdsGivenSelfIdAndRelationshipType(selfId, 'connection');
 
-exports.getRejects = (selfId) => {
-  return getUserIdsGivenSelfIdAndRelationshipType(selfId, 'reject');
-};
+exports.getRejects = (selfId) => getUserIdsGivenSelfIdAndRelationshipType(selfId, 'reject');
 
-exports.getRequests = (selfId) => {
-  return getUserIdsGivenSelfIdAndRelationshipType(selfId, 'request');
-};
+exports.getRequests = (selfId) => getUserIdsGivenSelfIdAndRelationshipType(selfId, 'request');
 
