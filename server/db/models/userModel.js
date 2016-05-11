ca1cc7d7338d53649ad4071f2a3713d97f4d396e
Reina Mei <reinameic@gmail.com>
2016-05-10 21:06:36 -0700

    Rename user_description to description

--- server/db/models/userModel.js
+++ server/db/models/userModel.js
@@ -20,7 +20,7 @@ const Users = db.define('users',
     photo_url: {
       type: Sequelize.STRING,
     },
-    user_description: {
+    description: {
       type: Sequelize.STRING(256),
     },
     // would like to add counter cache for request count and connection count
