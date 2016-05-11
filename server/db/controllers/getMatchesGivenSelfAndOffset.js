0d977f0f474dec25811b977fe781179025915a88
Reina Mei <reinameic@gmail.com>
2016-05-10 19:37:51 -0700

    Add ids to queries

--- server/db/controllers/getMatchesGivenSelfAndOffset.js
+++ server/db/controllers/getMatchesGivenSelfAndOffset.js
@@ -4,24 +4,24 @@ const Sequelize = require('sequelize');
 module.exports = (self, offSet) => {
   const queryStr = `SELECT DISTINCT teach.teach_id FROM (
                       (
-                        SELECT users_languages_levels.user AS teach_id FROM users_languages_levels 
+                        SELECT users_languages_levels.user_id AS teach_id FROM users_languages_levels 
                           INNER JOIN languages_levels 
-                            ON users_languages_levels.languages_levels = languages_levels.id 
+                            ON users_languages_levels.language_level_id = languages_levels.id 
                           INNER JOIN languages 
-                            ON languages_levels.language = languages.id 
+                            ON languages_levels.language_id = languages.id 
                           INNER JOIN levels 
-                            ON languages_levels.level = levels.id 
+                            ON languages_levels.level_id = levels.id 
                           WHERE languages.name IN (${self.willLearn.map(language => language[0]).join(',')}) AND level.name IN ('fluent', 'native') 
                       ) AS teach 
                       INNER JOIN 
                       ( 
-                        SELECT users_languages_levels.user AS learn_id FROM users_languages_levels 
+                        SELECT users_languages_levels.user_id AS learn_id FROM users_languages_levels 
                           INNER JOIN languages_levels 
-                            ON users_languages_levels.languages_levels = languages_levels.id 
+                            ON users_languages_levels.language_level_id = languages_levels.id 
                           INNER JOIN languages 
-                            ON languages_levels.language = languages.id 
+                            ON languages_levels.language_id = languages.id 
                           INNER JOIN levels 
-                            ON languages_levels.level = levels.id 
+                            ON languages_levels.level_id = levels.id 
                           WHERE languages.name IN (${self.canTeach.map(language => language[0]).join(',')}) AND level.name IN ('basic', 'intermediate', 'advanced')
                       ) AS learn 
                         ON teach.teach_id = learn.learn_id 
