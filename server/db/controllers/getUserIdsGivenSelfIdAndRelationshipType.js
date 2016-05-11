a94d913378b5004bb1988945d746bc8bc4f96f70
Reina Mei <reinameic@gmail.com>
2016-05-11 10:41:39 -0700

    Return connection or request ids for given user

--- server/db/controllers/getUserIdsGivenSelfIdAndRelationshipType.js
+++ server/db/controllers/getUserIdsGivenSelfIdAndRelationshipType.js
@@ -2,10 +2,19 @@ const db = require('../db.js');
 const Sequelize = require('sequelize');
 
 const getUserIdsGivenSelfIdAndRelationshipType = (selfId, relationshipType) => {
-  const queryStr = `SELECT users.id FROM users 
-                      INNER JOIN relationships 
-                        ON users.id = relationships.user1Id OR users.id = relationships.user2Id 
-                      WHERE users.id = ${selfId} AND relationships.type = ${relationshipType}
+  const queryStr = `(
+                      SELECT relationships.user1Id FROM users 
+                        INNER JOIN relationships 
+                          ON users.id = relationships.user2Id 
+                        WHERE users.id = ${selfId} AND relationships.type = ${relationshipType}
+                    )
+                    UNION 
+                    (
+                      SELECT relationships.user2Id FROM users 
+                        INNER JOIN relationships 
+                          ON users.id = relationships.user1Id 
+                        WHERE users.id = ${selfId} AND relationships.type = ${relationshipType}
+                    )
                     `;
   return db.query(queryStr, { type: Sequelize.QueryTypes.SELECT });
 };
