3e17290560a482c56f3a082d296e50c4cfe73918
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-05-10 21:34:40 -0700

    Rename query function to specify input fbId not fbProfile

--- server/db/controllers/getSelfBasicInfoGivenFBProfile.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db.js');
-
-module.exports = (facebookId) => {
-  const query = `SELECT * FROM users WHERE facebook_id = ${facebookId}`;
-  return db.query(query)
-    .spread((results, metadata) => results);
-};
-
