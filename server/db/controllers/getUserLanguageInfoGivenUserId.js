8e6aa36c2621e48a508afd3fdb790e01fdfe9a34
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-05-12 11:11:09 -0700

    Partially complete language handler

--- server/db/controllers/getUserLanguageInfoGivenUserId.js
+++ server/db/controllers/getUserLanguageInfoGivenUserId.js
@@ -1,12 +1,22 @@
 const db = require('../db.js');
+const languagesStore = require('../seed/languages.js').store;
+const levelsStore = require('../seed/levels.js').store;
 
-const getLanguagesLevels = (userId) => {
-  const query = `SELECT offer_or_learn, language_level_id FROM users_languages_levels WHERE user_id = '${userId}'`;
+const getLanguagesLevelsIds = (userId) => {
+  const query = `SELECT offer_or_learn, language_level_id FROM users_languages_levels WHERE user_id = '${userId}';`;
   return db.query(query)
     .spread((results, metadata) => results);
 };
 
-module.exports = (userId) =>
-  getLanguagesLevels(userId)
-    .then(r => console.log(r));
+const getLanguagesLevels = (entries) => {
+  return entries.map((entry) => {
+      // console.log(entry)
+      const query = `SELECT language_id, level_id FROM languages_levels WHERE id = '${entry.language_level_id}';`;
+      return db.query(query)
+        .spread((results, metadata) => results);
+  });
+};
 
+module.exports = (userId) =>
+  getLanguagesLevelsIds(userId)
+  .then(getLanguagesLevels);
--- server/utils/getSelfProfile.js
+++ server/utils/getSelfProfile.js
@@ -1,16 +1,34 @@
-const getBasicInfo = require('../db/controllers/getSelfBasicInfoGivenFBId.js');
+const getBasicInfo = require('../db/controllers/getUserBasicInfoGivenFBId.js');
+const getLanguageInfo = require('../db/controllers/getUserLanguageInfoGivenUserId.js');
+
+var assignBasicInfoToReq = (user, req) => {
+  req.idioma.profile = {
+    id: user.id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    description: user.description,
+    photoUrl: user.photo_url,
+  };
+  return user.id;
+};
+
+var assignLanguageInfoToReq = results => {
+  const user = results[0];
+  req.idioma.profile = {
+    id: user.id,
+    firstName: user.first_name,
+    lastName: user.last_name,
+    description: user.description,
+    photoUrl: user.photo_url,
+  };
+};
 
 module.exports = (req, res, next) => {
-  console.log('reached');
-  const fbId = req.user.id;
-  getBasicInfo(fbId).then(results => {
-    const user = results[0];
-    req.idioma.profile = {
-      id: user.id,
-      firstName: user.first_name,
-      lastName: user.last_name,
-      description: user.description,
-      photoUrl: user.photo_url,
-    };
-  }).then(next());
+  // const fbId = req.user.id;
+  const fbId = '11111';
+  getBasicInfo(fbId)
+  .then(results => assignBasicInfoToReq(results[0], req))
+  .then(getLanguageInfo)
+  .then(results => console.log('herreererere', results))
+  .then(next());
 };
