3c707a015be0edf2705779befe2cae7865c9eaa0
Reina Mei <reinameic@gmail.com>
2016-05-12 12:33:10 -0700

    Implement updateUserRoomTimeGivenUserIdAndRoomId

--- server/db/controllers/updateUserRoomTimeGivenUserIdAndRoomId.js
+++ server/db/controllers/updateUserRoomTimeGivenUserIdAndRoomId.js
@@ -1,3 +1,11 @@
 const db = require('../db.js');
 
 //this is used when a user is about to leave a room or close the tab (think of it as "last seen in this room")
+module.exports = (userId, roomId) => {
+  const queryStr = `
+    UPDATE users_rooms 
+      SET updatedAt = CURRENT_TIMESTAMP  
+      WHERE room_id = '${roomId}' AND user_id = '${userId}' 
+  `;
+  return db.query(queryStr).spread(results => results);
+};
