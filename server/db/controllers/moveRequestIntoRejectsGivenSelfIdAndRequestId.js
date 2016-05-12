c457e265048011e6e3084c8b08eb3d234cdc5685
Reina Mei <reinameic@gmail.com>
2016-05-11 19:02:41 -0700

    Implement moveRequestIntoRejectsGivenSelfIdAndRequestId

---
+++ server/db/controllers/moveRequestIntoRejectsGivenSelfIdAndRequestId.js
@@ -0,0 +1,10 @@
+const db = require('../db.js');
+
+module.exports = (selfId, requestId) => {
+  const queryStr = `
+                    UPDATE relationships 
+                      SET type = 'reject' 
+                      WHERE user1Id = '${requestId}' AND user2Id = '${selfId}'
+                  `;
+  return db.query(queryStr).spread((results, metadata) => results);
+};
