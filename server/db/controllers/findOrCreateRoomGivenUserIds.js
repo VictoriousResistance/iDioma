874a6c0fd9d6b9e98eb6ad729e0b31623294fb89
Reina Mei <reinameic@gmail.com>
2016-05-11 21:01:20 -0700

    Fix bug

--- server/db/controllers/findOrCreateRoomGivenUserIds.js
+++ server/db/controllers/findOrCreateRoomGivenUserIds.js
@@ -7,15 +7,15 @@ module.exports = (user1Id, user2Id) => {
   const queryStr = `
                     SELECT ur_1.ur1_id FROM ( 
                       (
-                        SELECT user_id AS user1_id, id AS ur1_id FROM users_rooms 
+                        SELECT user_id AS user1_id, room1_id AS ur1_id FROM users_rooms 
                           WHERE user_id = '${user1Id}' 
                       ) AS ur_1 
                       INNER JOIN 
                       (
-                        SELECT user_id AS user2_id, id AS ur2_id FROM users_rooms 
+                        SELECT user_id AS user2_id, room2_id AS ur2_id FROM users_rooms 
                           WHERE user_id = '${user2Id}' 
                       ) AS ur_2 
-                      ON ur_1.user1_id = ur_2.user2_id 
+                      ON ur_1.room1_id = ur_2.room2_id 
                     )
                     `;
   return db.query(queryStr).spread((results, metadata) => {
