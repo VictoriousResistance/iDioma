4814ba3513f428d8e8435a4c269d4a1cb2a54e40
Reina Mei <reinameic@gmail.com>
2016-05-12 11:51:30 -0700

    Implement leaveRoomGivenUserIdAndRoomId

---
+++ server/db/controllers/leaveRoomGivenUserIdAndRoomId.js
@@ -0,0 +1,40 @@
+const db = require('../db.js');
+
+module.exports = (userId, roomId) => {
+  const findNumberActiveParticipantsQueryStr = `
+    SELECT number_active_participants FROM rooms 
+      WHERE id = '${roomId}'
+  `;
+  const removeUserRoomsQueryStr = `
+    DELETE FROM users_rooms 
+      WHERE room_id = '${roomId}'
+  `;
+  const removeRoomQueryStr = `
+    DELETE FROM rooms 
+      WHERE id = '${roomId}'
+  `;
+  const hideUserRoomQueryStr = `
+    UPDATE users_rooms 
+      SET \`show\` = false 
+      WHERE user_id = '${userId}' AND room_id = '${roomId}'
+  `;
+  const reduceNumberActiveParticipantsQueryStr = `
+    UPDATE rooms 
+      SET \`number_active_participants\` = \`number_active_participants\` - 1 
+      WHERE id = '${roomId}'
+  `;
+  return db.query(findNumberActiveParticipantsQueryStr).spread(results => {
+    if (results[0] == 1) {
+      return db.query(removeUserRoomsQueryStr)
+               .spread(() => (
+                 db.query(removeRoomQueryStr)
+                   .spread(room => room)
+               ));
+    }
+    return db.query(hideUserRoomQueryStr)
+             .spread(() => (
+                db.query(reduceNumberActiveParticipantsQueryStr)
+                  .spread(room => room)
+              ));
+  });
+};
