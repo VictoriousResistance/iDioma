c4ee5af9e17ea7afa9733cfc25aadeeaf7ddf781
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-05-11 19:24:40 -0700

    Rename controller for general case

--- server/db/controllers/getSelfBasicInfoGivenUserId.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db.js');
-
-module.exports = (userId) => {
-  const query = `SELECT * FROM users WHERE id = '${userId}'`;
-  return db.query(query)
-    .spread((results, metadata) => results);
-};
-
