696a8289ec4d1ee201c03a91ce68982d4428a5be
Reina Mei <reinameic@gmail.com>
2016-05-12 20:39:44 -0700

    Improve promise readability

--- server/db/controllers/updateLanguagesGivenTypeAndUserId.js
+++ server/db/controllers/updateLanguagesGivenTypeAndUserId.js
@@ -8,27 +8,24 @@ module.exports = (type, userId, languageLevels) => ( // languageLevels is an arr
   db.query(`
     DELETE FROM users_languages_levels 
       WHERE user_id = '${userId}'
-  `)
-  .spread(results => results)
-  .then(() => (
-    Users.findById(userId)
-      .then(user => {
-        const languageLevelQueries = languageLevels.map(languageLevel => (
-          LanguagesLevels.findOrCreate({
-            where: {
-              language_id: languageToId[languageLevel[0]],
-              level_id: levelToId[languageLevel[1]],
-            },
-          })
-          .spread(results => results)
-        ));
-        return Promise.all(languageLevelQueries)
-          .then(lls => {
-            const addQueries = lls.map(ll => (
-              user.addLanguagesLevels(ll, { type })
-            ));
-            return Promise.all(addQueries);
-          });
+  `) // delete existing associations between user and any languageLevels
+  .spread(() => Users.findById(userId))
+  .then(user => {
+    const languageLevelQueries = languageLevels.map(languageLevel => (
+      LanguagesLevels.findOrCreate({
+        where: {
+          language_id: languageToId[languageLevel[0]],
+          level_id: levelToId[languageLevel[1]],
+        },
       })
-  ))
+      .spread(result => result) // each result is an instance of the LanguagesLevels sequelize model
+    ));
+    return Promise.all(languageLevelQueries)
+      .then(lls => {
+        const addQueries = lls.map(ll => (
+          user.addLanguagesLevels(ll, { offer_or_learn: type })
+        ));
+        return Promise.all(addQueries);
+      });
+  })
 );
