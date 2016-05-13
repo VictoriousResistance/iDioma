e42bd53426d18c24f108aec8e3cae9e2c69c90c8
Reina Mei <reinameic@gmail.com>
2016-05-13 10:36:52 -0700

    Handle put to /api/relationships

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,9 +2,9 @@ const apiRouter = require('express').Router();
 const getMatches = require('../db/controllers/getMatchesGivenSelfAndOffset.js');
 const addToRequests = require('../db/controllers/moveMatchIntoRequestsGivenSelfIdAndMatchId.js');
 const addToRejects = require('../db/controllers/moveMatchIntoRejectsGivenSelfIdAndMatchId.js');
+const changeToRejectFromConnection = require('../db/controllers/moveConnectionIntoRejectsGivenSelfIdAndConnectionId.js');
 const changeToConnectionFromRequest = require('../db/controllers/moveRequestIntoConnectionsGivenSelfIdAndRequestId.js');
 const changeToRejectFromRequest = require('../db/controllers/moveRequestIntoRejectsGivenSelfIdAndRequestId.js');
-const changeToRejectFromConnection = require('../db/controllers/moveConnectionIntoRejectsGivenSelfIdAndConnectionId.js');
 
 apiRouter.route('/matches')
   .get((req, res) => { // get matches
@@ -18,20 +18,41 @@ apiRouter.route('/relationships')
   .post((req, res) => { // post to rejects and requests
     if (req.body.newType === 'request') {
       return addToRequests(req.body.selfId, req.body.matchId)
-      .then(() => {
-        res.sendStatus(201);
-      });
+        .then(() => {
+          res.sendStatus(201);
+        });
     }
     if (req.body.newType === 'reject') {
       return addToRejects(req.body.selfId, req.body.matchId)
-      .then(() => {
-        res.sendStatus(201);
-      });
+        .then(() => {
+          res.sendStatus(201);
+        });
     }
     return res.sendStatus(404);
   })
   .put((req, res) => { // change relationship type
-
+    if (req.body.oldType === 'connection') {
+      return changeToRejectFromConnection(req.body.selfId, req.body.connectionId)
+        .then(() => {
+          res.sendStatus(200);
+        });
+    }
+    if (req.body.oldType === 'request') {
+      if (req.body.newType === 'connection') {
+        return changeToConnectionFromRequest(req.body.selfId, req.body.requestId)
+          .then(() => {
+            res.sendStatus(200);
+          });
+      }
+      if (req.body.newType === 'reject') {
+        return changeToRejectFromRequest(req.body.selfId, req.body.requestId)
+          .then(() => {
+            res.sendStatus(200);
+          });
+      }
+      return res.sendStatus(404);
+    }
+    return res.sendStatus(404);
   });
 
 apiRouter.route('/profile/:id')
