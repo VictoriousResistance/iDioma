cedc08a645120e467ffd96f78b885d28ee45521a
Reina Mei <reinameic@gmail.com>
2016-05-12 20:19:50 -0700

    Implement updateLanguagesGivenTypeAndUserId

--- server/db/controllers/updateLanguagesGivenTypeAndUserId.js
+++ server/db/controllers/updateLanguagesGivenTypeAndUserId.js
@@ -1,20 +1,34 @@
 const db = require('../db.js');
 const languageToId = require('../seed/languages.js').languageToId;
 const levelToId = require('../seed/levels.js').levelToId;
+const LanguagesLevels = require('../models/languageLevelModel.js');
+const Users = require('../models/userModel.js');
 
-module.exports = (type, userId, languageLevels) => { //languageLevels is an array of tuples in the form of [language, level]
-  //find if that language_level already exists
-  const languageLevelQueries = languageLevels.map(languageLevel => (
-    db.query(`
-      SELECT id FROM languages_levels 
-        WHERE language_id = '${languageToId[languageLevel[0]]}' AND level_id = '${levelToId[languageLevel[1]]}'
-    `)
-  ));
-
-  Promise.all(languageLevelQueries)
-    .then( languageLevelIds => {
-      
-    })
-
-
-};
+module.exports = (type, userId, languageLevels) => ( // languageLevels is an array of tuples in the form of [language, level]
+  db.query(`
+    DELETE FROM users_languages_levels 
+      WHERE user_id = '${userId}'
+  `)
+  .spread(results => results)
+  .then(() => (
+    Users.findById(userId)
+      .then(user => {
+        const languageLevelQueries = languageLevels.map(languageLevel => (
+          LanguagesLevels.findOrCreate({
+            where: {
+              language_id: languageToId[languageLevel[0]],
+              level_id: levelToId[languageLevel[1]],
+            },
+          })
+          .spread(results => results)
+        ));
+        return Promise.all(languageLevelQueries)
+          .then(lls => {
+            const addQueries = lls.map(ll => (
+              user.addLanguagesLevels(ll, { type })
+            ));
+            return Promise.all(addQueries);
+          });
+      })
+  ))
+);
