a505e71d1c82a211ec31e4359d5784a356fee9c5
Reina Mei <reinameic@gmail.com>
2016-05-13 10:06:06 -0700

    Require apiRouter in server

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -1,13 +1,21 @@
 const apiRouter = require('express').Router();
 
-apiRouter.route('/api/users')
+apiRouter.route('/matches')
   .get((req, res) => { // get matches
 
-  })
+  });
 
-apiRouter.route('/api/relationships')
-  .post() // post to rejects and requests
-  .put()  // change relationship type
+apiRouter.route('/relationships')
+  .post((req, res) => {
 
-apiRouter.route('/api/profile/:id')
-  .put() // update profile
+  }) // post to rejects and requests
+  .put((req, res) => {
+
+  });  // change relationship type
+
+apiRouter.route('/profile/:id')
+  .put((req, res) => {
+
+  }); // update profile
+
+module.exports = apiRouter;
