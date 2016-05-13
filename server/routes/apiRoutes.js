85e89e217cbcdc3211362bdeb00514b5b7dbf3a4
Reina Mei <reinameic@gmail.com>
2016-05-13 10:19:26 -0700

    Handle post to /api/relationships

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -10,13 +10,25 @@ apiRouter.route('/matches')
   .get((req, res) => { // get matches
     getMatches(req.body.id, req.body.offset)
     .then(matches => {
-      res.json = matches;
+      res.json(matches);
     });
   });
 
 apiRouter.route('/relationships')
   .post((req, res) => {
-
+    if (req.body.newType === 'request') {
+      return addToRequests(req.body.selfId, req.body.matchId)
+      .then(() => {
+        res.sendStatus(201);
+      });
+    }
+    if (req.body.newType === 'reject') {
+      return addToRejects(req.body.selfId, req.body.matchId)
+      .then(() => {
+        res.sendStatus(201);
+      });
+    }
+    return res.sendStatus(404);
   }) // post to rejects and requests
   .put((req, res) => {
 
