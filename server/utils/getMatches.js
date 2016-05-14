5c2bbed22136d1e4c34fb37b60b3612bb977d9ee
Reina Mei <reinameic@gmail.com>
2016-05-14 14:57:05 -0700

    Fix typo

--- server/utils/getMatches.js
+++ server/utils/getMatches.js
@@ -3,13 +3,10 @@ const getMatches = require('../db/controllers/getMatchesGivenSelfAndOffset');
 module.exports = (req, res, next) => {
   const self = req.idioma.profile;
 
-  // set offset to 20 on req
-  req.idioma.matches.offSet = 0;
-  const offSet = req.idioma.matches.offSet;
-
-  getMatches(self, offSet)
+  getMatches(self, 0)
   .then((matches) => {
     req.idioma.matches.values = matches;
+    req.idioma.matches.offset = 20;
   })
   .then(() => next())
   .catch((error) => {
