8318888a451cf3856583354625468a8ba30ac668
Reina Mei <reinameic@gmail.com>
2016-05-15 15:01:17 -0700

    Refactor twilio route to use user-supplied identity

--- server/routes/twilio.js
+++ server/routes/twilio.js
@@ -4,29 +4,27 @@ username for the client requesting a token, and takes a device ID as a query
 parameter.
 */
 module.exports = (app) => {
-  app.get('/token', function(request, response) {
-      var identity = randomUsername();
-      
-      // Create an access token which we will sign and return to the client,
-      // containing the grant we just created
-      var token = new AccessToken(
-          process.env.TWILIO_ACCOUNT_SID,
-          process.env.TWILIO_API_KEY,
-          process.env.TWILIO_API_SECRET
-      );
+  app.get('/token', (req, res) => {
+    const identity = req.body.identity;
+    // containing the grant we just created
+    var token = new AccessToken(
+        process.env.TWILIO_ACCOUNT_SID,
+        process.env.TWILIO_API_KEY,
+        process.env.TWILIO_API_SECRET
+    );
 
-      // Assign the generated identity to the token
-      token.identity = identity;
+    // Assign the generated identity to the token
+    token.identity = identity;
 
-      //grant the access token Twilio Video capabilities
-      var grant = new ConversationsGrant();
-      grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
-      token.addGrant(grant);
+    //grant the access token Twilio Video capabilities
+    var grant = new ConversationsGrant();
+    grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
+    token.addGrant(grant);
 
-      // Serialize the token to a JWT string and include it in a JSON response
-      response.send({
-          identity: identity,
-          token: token.toJwt()
-      });
+    // Serialize the token to a JWT string and include it in a JSON response
+    res.send({
+        identity,
+        token: token.toJwt(),
+    });
   });
 };
