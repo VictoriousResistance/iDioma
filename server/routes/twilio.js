78e13b903b2745142c3df61f57217d20f153b8f1
Reina Mei <reinameic@gmail.com>
2016-05-15 15:03:44 -0700

    Add dependencies in twilio route

--- server/routes/twilio.js
+++ server/routes/twilio.js
@@ -1,13 +1,19 @@
+require('dotenv').load();
+
+const AccessToken = require('twilio').AccessToken;
+const ConversationsGrant = AccessToken.ConversationsGrant;
+
 /*
 Generate an Access Token for a chat application user - it generates a random
 username for the client requesting a token, and takes a device ID as a query
 parameter.
 */
+
 module.exports = (app) => {
   app.get('/token', (req, res) => {
     const identity = req.body.identity;
     // containing the grant we just created
-    var token = new AccessToken(
+    const token = new AccessToken(
         process.env.TWILIO_ACCOUNT_SID,
         process.env.TWILIO_API_KEY,
         process.env.TWILIO_API_SECRET
@@ -16,15 +22,15 @@ module.exports = (app) => {
     // Assign the generated identity to the token
     token.identity = identity;
 
-    //grant the access token Twilio Video capabilities
-    var grant = new ConversationsGrant();
+    // grant the access token Twilio Video capabilities
+    const grant = new ConversationsGrant();
     grant.configurationProfileSid = process.env.TWILIO_CONFIGURATION_SID;
     token.addGrant(grant);
 
     // Serialize the token to a JWT string and include it in a JSON response
     res.send({
-        identity,
-        token: token.toJwt(),
+      identity,
+      token: token.toJwt(),
     });
   });
 };
