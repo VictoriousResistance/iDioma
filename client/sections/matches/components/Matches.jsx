01be21ffd0920bfae36895b41471e97aff2afbb6
Reina Mei <reinameic@gmail.com>
2016-05-17 15:30:23 -0700

    Reload page after updating profile

--- client/sections/matches/components/Matches.jsx
+++ client/sections/matches/components/Matches.jsx
@@ -44,7 +44,11 @@ const Matches = ({ matches, onConnectClick, onHideClick, offset, onLoadMoreClick
         </div>
       );
     }
-    return <div className="empty-tab-message">You don't have any matches at the moment. Come back later and check again!</div>;
+    return (
+      <div className="empty-tab-message">
+        <p>You don't have any matches at the moment. Come back later and check again!</p>
+      </div>
+    );
   }
   const loadMore = offset > matches.length ? null : (<button href="#" className="load" onClick={() => { onLoadMoreClick(self, offset); }}>Load More</button>); 
   return (
