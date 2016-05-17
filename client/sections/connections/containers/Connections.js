8fab2011b1c53d6a211e0c533dcd3d1edcb0bf45
Muhammad Naqvi <naqvi.muhammad@gmail.com>
2016-05-17 11:32:42 -0700

    Change endpoint for starting a room

--- client/sections/connections/containers/Connections.js
+++ client/sections/connections/containers/Connections.js
@@ -15,7 +15,7 @@ const mapStateToProps = (state) => (
 const mapDispatchToProps = (dispatch) => (
   {
     onNewConvo: (selfId, connection, currentRooms) => {
-      request('POST', '/api/relationships', {
+      request('POST', '/api/rooms', {
         json: {
           selfId,
           connectionId: connection.id,
