b054529058be21c8096756b04c34f1630ceeb9f4
Reina Mei <reinameic@gmail.com>
2016-05-18 16:26:41 -0700

    Configure db host for deployment

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,8 @@
-var Sequelize = require('sequelize');
-var password = require('./config.js');
+const Sequelize = require('sequelize');
+const password = require('./config.js');
 
-var db = new Sequelize('idioma', 'root', password);
+const db = new Sequelize('idioma', 'iDioma', password, {
+  host: 'idioma-production.cxdvnhbvpuxn.us-west-2.rds.amazonaws.com'
+});
 
 module.exports = db;
