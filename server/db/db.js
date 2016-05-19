34bf3e011e9b891d1280dff65994d2fa12c4bb64
Reina Mei <reinameic@gmail.com>
2016-05-18 18:49:04 -0700

    Configure db for dev and prod

--- server/db/db.js
+++ server/db/db.js
@@ -1,8 +1,15 @@
 const Sequelize = require('sequelize');
 const password = require('./config.js');
 
-const db = new Sequelize('idioma', 'iDioma', password, {
-  host: 'idioma-production.cxdvnhbvpuxn.us-west-2.rds.amazonaws.com'
-});
+var db;
+
+if (process.env.PORT) {
+  db = new Sequelize('idioma', 'iDioma', password, {
+    host: 'idioma-production.cxdvnhbvpuxn.us-west-2.rds.amazonaws.com',
+  });
+} else {
+  db = new Sequelize('idioma', 'root', password);
+}
+
 
 module.exports = db;
